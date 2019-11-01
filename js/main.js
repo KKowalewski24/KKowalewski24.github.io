2e79c83985ed797be8bad284caf3197b235010be
Kamil Kowalewski <szamil128@onet.pl>
2019-11-01 20:30:38 +0100

    Rewrite main.js, close #2

--- js/main.js
+++ js/main.js
@@ -1,63 +1,69 @@
-(function ($) {
-  "use strict"; // Start of use strict
+"use strict";
 
-  // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: (target.offset().top - 71)
-        }, 1000, "easeInOutExpo");
-        return false;
-      }
+/*------------------------ VARIABLE ------------------------*/
+const htmlBody = $("html, body");
+const body = $("body");
+const aJsScrollTrigger = $('a.js-scroll-trigger[href*="#"]:not([href="#"])');
+const scrollToTop = $(".scroll-to-top");
+const jsScrollTrigger = $(".js-scroll-trigger");
+const navbarCollapse = $(".navbar-collapse");
+const mainNav = $("#mainNav");
+
+const OFFSET_71 = 71;
+const OFFSET_80 = 80;
+const OFFSET_100 = 100;
+const TIMEOUT = 1000;
+
+/*------------------------ FUNCTIONS ------------------------*/
+
+// Scroll to top button appear
+$(document).scroll(function () {
+  let scrollDistance = $(this).scrollTop();
+
+  if (scrollDistance > 100) {
+    scrollToTop.fadeIn();
+  } else {
+    scrollToTop.fadeOut();
+  }
+});
+
+// Smooth scrolling using jQuery easing
+aJsScrollTrigger.click(function () {
+  if (location.pathname.replace(/^\//, '')
+    === this.pathname.replace(/^\//, '')
+    && location.hostname === this.hostname) {
+    let target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+
+    if (target.length) {
+      htmlBody.animate({
+        scrollTop: (target.offset().top - OFFSET_71)
+      }, TIMEOUT, "easeInOutExpo");
+
+      return false;
     }
-  });
+  }
+});
 
-  // Scroll to top button appear
-  $(document).scroll(function () {
-    var scrollDistance = $(this).scrollTop();
-    if (scrollDistance > 100) {
-      $('.scroll-to-top').fadeIn();
-    } else {
-      $('.scroll-to-top').fadeOut();
-    }
-  });
+// Closes responsive menu when a scroll trigger link is clicked
+jsScrollTrigger.click(function () {
+  navbarCollapse.collapse("hide");
+});
 
-  // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function () {
-    $('.navbar-collapse').collapse('hide');
-  });
+// Activate scrollspy to add active class to navbar items on scroll
+body.scrollspy({
+  target: '#mainNav',
+  offset: OFFSET_80
+});
 
-  // Activate scrollspy to add active class to navbar items on scroll
-  $('body').scrollspy({
-    target: '#mainNav',
-    offset: 80
-  });
+let collapseNavigationBar = () => {
+  if (mainNav.offset.top > 100) {
+    mainNav.addClass("navbar-shrink");
+  } else {
+    mainNav.removeClass("navbar-shrink");
+  }
+};
 
-  // Collapse Navbar
-  var navbarCollapse = function () {
-    if ($("#mainNav").offset().top > 100) {
-      $("#mainNav").addClass("navbar-shrink");
-    } else {
-      $("#mainNav").removeClass("navbar-shrink");
-    }
-  };
-  // Collapse now if page is not at top
-  navbarCollapse();
-  // Collapse the navbar when page is scrolled
-  $(window).scroll(navbarCollapse);
-
-  // Floating label headings for the contact form
-  $(function () {
-    $("body").on("input propertychange", ".floating-label-form-group", function (e) {
-      $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
-    }).on("focus", ".floating-label-form-group", function () {
-      $(this).addClass("floating-label-form-group-with-focus");
-    }).on("blur", ".floating-label-form-group", function () {
-      $(this).removeClass("floating-label-form-group-with-focus");
-    });
-  });
-
-})(jQuery); // End of use strict
+/*------------------------ FUNCTION CALL ------------------------*/
+collapseNavigationBar();
+$(window).scroll(collapseNavigationBar);
